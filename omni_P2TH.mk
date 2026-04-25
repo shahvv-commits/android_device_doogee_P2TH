f0fca5211bf7480563c37085b03b65857e5dc642
shahvv-commits <shahvv@gmail.com>
2026-04-25 22:46:09 +0100

    Update omni_P2TH.mk

--- omni_P2TH.mk
+++ omni_P2TH.mk
@@ -1,5 +1,9 @@
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from TWRP configuration
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from P2TH device
 $(call inherit-product, device/doogee/P2TH/device.mk)
