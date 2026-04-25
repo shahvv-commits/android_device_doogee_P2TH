a017d4a0361a05914fcb269b1ac26d40387cbb39
shahvv-commits <shahvv@gmail.com>
2026-04-25 19:45:37 +0100

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2026 The Android Open Source Project
-# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_P2TH.mk
 
