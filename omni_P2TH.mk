3133774a17c54c468f456cbd9c88bd6be0824263
shahvv-commits <shahvv@gmail.com>
2026-04-26 19:02:23 +0100

    Update omni_P2TH.mk

--- omni_P2TH.mk
+++ omni_P2TH.mk
@@ -1,6 +1,6 @@
 # Inherit from those products. Most specific first.
-$(call inherit-product, build/make/target/product/full_base_telephony.mk)
-$(call inherit-product, build/make/target/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from P2TH device
 $(call inherit-product, device/doogee/P2TH/device.mk)
@@ -17,7 +17,7 @@ PRODUCT_MANUFACTURER := doogee
 
 PRODUCT_GMS_CLIENTID_BASE := android-doogee
 
-# Build Flags for Android 12 GKI
+# Build Flags
 PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
 PRODUCT_BUILD_RECOVERY_IMAGE := true
 
