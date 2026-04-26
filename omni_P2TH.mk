a62849ab4666147e542690bdf9847625f9a421f4
shahvv-commits <shahvv@gmail.com>
2026-04-26 14:55:10 +0100

    Update omni_P2TH.mk

--- omni_P2TH.mk
+++ omni_P2TH.mk
@@ -2,11 +2,19 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# Inherit from GKI ramdisk (Required for Android 12+ vendor_boot)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
+
 # Inherit from TWRP configuration
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from P2TH device
 $(call inherit-product, device/doogee/P2TH/device.mk)
+
+# --- GKI / Vendor Boot Packaging Flags ---
+PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
+PRODUCT_BUILD_RECOVERY_IMAGE := false
+# -----------------------------------------
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := P2TH
