0ed1ae7f2ea82ef8f2286d0baa89df254867aacf
shahvv-commits <shahvv@gmail.com>
2026-04-26 22:30:43 +0100

    Update omni_P2TH.mk

--- omni_P2TH.mk
+++ omni_P2TH.mk
@@ -1,25 +1,12 @@
-# Device identifier
 PRODUCT_DEVICE := P2TH
 PRODUCT_NAME := omni_P2TH
 PRODUCT_BRAND := Doogee
 PRODUCT_MODEL := P2TH
 PRODUCT_MANUFACTURER := doogee
-
-PRODUCT_GMS_CLIENTID_BASE := android-doogee
-
-# GKI Build Flags
-PRODUCT_BUILD_VENDOR_BOOT_IMAGE := true
-PRODUCT_BUILD_RECOVERY_IMAGE := true
-
-# Platform
 PRODUCT_PLATFORM := mt6789
 
-# Inherit from those products. Most specific first.
+# Using the most basic relative path possible
 $(call inherit-product, build/make/target/product/core_64bit.mk)
 $(call inherit-product, build/make/target/product/full_base_telephony.mk)
-
-# Inherit from P2TH device configuration
 $(call inherit-product, device/doogee/P2TH/device.mk)
-
-# Inherit TWRP common configuration
 $(call inherit-product, vendor/twrp/config/twrp.mk)
