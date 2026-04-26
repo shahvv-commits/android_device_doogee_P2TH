0cc96e3d608e480da995a5f03daeeca049ae341e
shahvv-commits <shahvv@gmail.com>
2026-04-26 09:48:10 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,10 +8,15 @@
 ALLOW_MISSING_DEPENDENCIES := true
 DEVICE_PATH := device/doogee/P2TH
 
-# Force Vendor Boot Flags (GKI Compatibility)
+# Force Vendor Boot Flags (The mandatory pair)
 BOARD_USES_VENDOR_BOOTIMAGE := true
 BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
+BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
+
+# Global Build Flags
 BOARD_USES_RECOVERY_AS_BOOT := false
+AB_OTA_UPDATER := true
+TARGET_NO_RECOVERY := true
 
 # Logic for GKI/Header Version 4
 AB_OTA_UPDATER := true
