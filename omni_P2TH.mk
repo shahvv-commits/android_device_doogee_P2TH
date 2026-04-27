aea92167b88aaf30bb395fcf2d778cde8e1685ff
shahvv-commits <shahvv@gmail.com>
2026-04-27 20:00:45 +0100

    Update omni_P2TH.mk

--- omni_P2TH.mk
+++ omni_P2TH.mk
@@ -6,10 +6,10 @@ PRODUCT_MODEL := P2TH
 PRODUCT_MANUFACTURER := doogee
 PRODUCT_PLATFORM := mt6789
 
-# Standard AOSP inheritance. 
-# We use $(gettop) to ensure the path is absolute from the workspace root.
-$(call inherit-product, $(gettop)/build/make/target/product/core_64bit.mk)
-$(call inherit-product, $(gettop)/build/make/target/product/full_base_telephony.mk)
+# Standard AOSP inheritance paths.
+# Build #106 YAML will physically place 'target' in the root to match these lines.
+$(call inherit-product, target/product/core_64bit.mk)
+$(call inherit-product, target/product/full_base_telephony.mk)
 
 # Inherit from P2TH device configuration
 $(call inherit-product, device/doogee/P2TH/device.mk)
