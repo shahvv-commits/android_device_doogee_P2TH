e552484aec2416b6a9cef085f1c6c50bf2ebbaaa
shahvv-commits <shahvv@gmail.com>
2026-04-27 22:44:17 +0100

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -10,3 +10,11 @@ LOCAL_PATH := $(call my-dir)
 ifeq ($(TARGET_DEVICE),P2TH)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
+
+# Copy the DTB image to the output directory
+include $(CLEAR_VARS)
+LOCAL_MODULE := dtb.img
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := prebuilt/dtb.img
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)
+include $(BUILD_PREBUILT)
